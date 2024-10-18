05351e7bedcdf64ebd8ee15bcd98108a6e272384
Leopladom <114880850+Leopladom@users.noreply.github.com>
2024-10-18 12:49:28 -0300

    .

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-u2cy_*yur-^)a-069jr*k^ap0lz!07%iqlm_sc*+3mcwf8-)iw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.244', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.50', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -80,9 +80,9 @@ DATABASES = {
 
 'default': {
 'ENGINE': 'django.db.backends.mysql',
-'NAME': 'mercadolibro',
+'NAME': 'mercadolibro1',
 'USER': 'root',
-'PASSWORD': '',
+'PASSWORD': 'Leon4350',
 'HOST': 'localhost',
 'PORT': '3306',
 'OPTIONS': {
